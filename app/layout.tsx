67878068b159915cb6956ef2cb6012a45149f0bb
Mohamed Elkholy <mkh117@gmail.com>
2026-02-24 23:40:42 +0400

    feat: integrate Vercel Analytics into root layout
    Signed-off-by: Mohamed Elkholy <mkh117@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/next"
 import { ThemeProvider } from '@/components/theme-provider'
 import { ToasterProvider } from '@/components/toaster-provider'
 import { TooltipProvider } from '@/components/ui/tooltip'
@@ -33,6 +34,7 @@ export default async function RootLayout({ children }: LayoutProps<'/'>) {
           <ToasterProvider position='top-center' />
           <TooltipProvider>{children}</TooltipProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
